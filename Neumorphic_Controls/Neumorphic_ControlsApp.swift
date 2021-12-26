be583868180ffb2994f502f1323db8c9c6abfb6c
Stanley Pan <imoment.developer@gmail.com>
2021-12-26 13:37:39 -0500

    Initial Commit

---
+++ Neumorphic_Controls/Neumorphic_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphic_ControlsApp.swift
+//  Neumorphic_Controls
+//
+//  Created by Stanley Pan on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphic_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
